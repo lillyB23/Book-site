e37a1403c4fb622e4822b3e9ca7644d583109ce1
Gladys Desktop <lillyescobar7@gmail.com>
2023-06-23 20:35:20 -0500

    home, topbanner and footer components were created

--- book-site/src/app/app.module.ts
+++ book-site/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopbannerComponent } from './topbanner/topbanner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopbannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
